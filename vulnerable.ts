89d071338c46eab2131faeab992819368596d043
B0zal <me@btz.pm>
2023-08-29 12:28:18 +0700

    Added Vulnerable code for a test

---
+++ vulnerable.ts
@@ -0,0 +1,10 @@
+// this example vulnerable code for typescript/javascript it will trigger a CodeQL
+export function merge(target: any, source: any) {
+	Object.keys(source).forEach(function (key) {
+	  if (source[key] && typeof source[key] === "object") {
+		merge((target[key] = target[key] || {}), source[key]);
+		return;
+	  }
+	  target[key] = source[key];
+	});
+  }
