f230ed0d4730879384e98a458cd1a3b153d641f0
B0zal <me@btz.pm>
2023-08-30 18:38:11 +0700

    Create vulnerable.java

---
+++ vulnerable.java
@@ -0,0 +1,26 @@
+// test code
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class FileUploadController {
+    public void uploadFile(InputStream inputStream, String fileName) throws IOException {
+        String uploadDir = "/uploads/";
+        File file = new File(uploadDir + fileName);
+        
+        try (OutputStream outputStream = new FileOutputStream(file)) {
+            int read;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
